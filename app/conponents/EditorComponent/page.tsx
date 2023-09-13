e8880ef90d509a36400b4cbf19efdd463a8c9145
TKMhub <111867865+TKMhub@users.noreply.github.com>
2023-09-14 00:51:16 +0900

    style

--- app/conponents/EditorComponent/page.tsx
+++ app/conponents/EditorComponent/page.tsx
@@ -7,12 +7,19 @@ import FormatAlignJustifyIcon from "@mui/icons-material/FormatAlignJustify";
 import FormatBoldIcon from "@mui/icons-material/FormatBold";
 import FormatItalicIcon from "@mui/icons-material/FormatItalic";
 import FormatUnderlinedIcon from "@mui/icons-material/FormatUnderlined";
-// import FormatColorFillIcon from "@mui/icons-material/FormatColorFill";
-// import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+import AddIcon from "@mui/icons-material/Add";
+import PanoramaIcon from "@mui/icons-material/Panorama";
+import FormatColorFillIcon from "@mui/icons-material/FormatColorFill";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
 import Divider from "@mui/material/Divider";
 import Paper from "@mui/material/Paper";
 import ToggleButton from "@mui/material/ToggleButton";
 import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import { blue } from "@mui/material/colors";
+import { Button, TextField } from "@mui/material";
+
+import { collection, addDoc, getFirestore } from "firebase/firestore";
+import { RGIntegerFormat } from "three";
 
 const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
   "& .MuiToggleButtonGroup-grouped": {
@@ -31,25 +38,75 @@ const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
 }));
 
 export const EditorComponent = () => {
-  const [alignment, setAlignment] = React.useState("left");
-  const [formats, setFormats] = React.useState(() => ["italic"]);
+  const [alignment, setAlignment] =
+    React.useState<React.CSSProperties["textAlign"]>("left");
+  const [formats, setFormats] = React.useState<string[]>([]);
+  const [textContents, setTextContents] = React.useState("");
+  const [contentParts, setContentParts] = React.useState<any[]>([]);
+
+  // 新しい関数を追加してコンテンツパーツをハンドリング
+  const handleAddContentsParts = (
+    event: React.MouseEvent<HTMLElement>,
+    newFormats: string[]
+  ) => {
+    setContentParts([...contentParts, { formats: newFormats, text: "" }]);
+  };
+
+  // スタイルオブジェクトを作成
+  const textAreaStyle: React.CSSProperties = {
+    textAlign: alignment,
+    fontStyle: formats.includes("italic") ? "italic" : "normal",
+    fontWeight: formats.includes("bold") ? "700" : "normal",
+    textDecoration: formats.includes("underlined") ? "underline" : "none",
+  };
 
   const handleFormat = (
     event: React.MouseEvent<HTMLElement>,
     newFormats: string[]
   ) => {
-    setFormats(newFormats);
+    setFormats(newFormats); // newFormats を適用するように更新
   };
 
   const handleAlignment = (
     event: React.MouseEvent<HTMLElement>,
     newAlignment: string
   ) => {
-    setAlignment(newAlignment);
+    setAlignment(newAlignment as React.CSSProperties["textAlign"]); // newAlignment を適用するように更新
+  };
+
+  // handleAddContents関数でFirestoreへテキストを保存
+  const handleAddContents = async () => {
+    try {
+      const docRef = await addDoc(collection(getFirestore(), "posts"), {
+        text: textContents,
+        formats,
+        alignment,
+        createdAt: new Date(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
   };
 
   return (
-    <div>
+    <div
+      className="p-3 rounded-md bg-white shadow-md space-y-4"
+      style={{ height: "70vh" }}
+    >
+      <div>
+        {/* タイトル用 */}
+        <TextField
+          fullWidth
+          multiline
+          placeholder="タイトル"
+          rows={1}
+          variant="outlined"
+          value={textContents}
+          onChange={(e) => setTextContents(e.target.value)}
+          style={textAreaStyle} // スタイルオブジェクトを適用
+        />
+      </div>
       <Paper
         elevation={0}
         sx={{
@@ -58,6 +115,21 @@ export const EditorComponent = () => {
           flexWrap: "wrap",
         }}
       >
+        <StyledToggleButtonGroup
+          size="small"
+          // value={alignment}
+          exclusive
+          onChange={handleAddContentsParts}
+          aria-label="add Contents"
+        >
+          <ToggleButton value="left" aria-label="left aligned">
+            <AddIcon sx={{ color: blue[300] }} />
+          </ToggleButton>
+          <ToggleButton value="left" aria-label="left aligned">
+            <PanoramaIcon sx={{ color: blue[300] }} />
+          </ToggleButton>
+        </StyledToggleButtonGroup>
+        <Divider flexItem orientation="vertical" sx={{ mx: 0.5, my: 1 }} />
         <StyledToggleButtonGroup
           size="small"
           value={alignment}
@@ -94,12 +166,32 @@ export const EditorComponent = () => {
           <ToggleButton value="underlined" aria-label="underlined">
             <FormatUnderlinedIcon />
           </ToggleButton>
-          {/* <ToggleButton value="color" aria-label="color" disabled>
+          <ToggleButton value="color" aria-label="color" disabled>
             <FormatColorFillIcon />
             <ArrowDropDownIcon />
-          </ToggleButton> */}
+          </ToggleButton>
         </StyledToggleButtonGroup>
       </Paper>
+      <div>
+        <TextField
+          fullWidth
+          multiline
+          rows={20}
+          variant="outlined"
+          value={textContents}
+          onChange={(e) => setTextContents(e.target.value)}
+          style={textAreaStyle} // スタイルオブジェクトを適用
+          placeholder="本文"
+        />
+      </div>
+      <Button
+        variant="contained"
+        size="large"
+        style={{ textAlign: "right" }}
+        onClick={handleAddContents}
+      >
+        投稿
+      </Button>
     </div>
   );
 };
